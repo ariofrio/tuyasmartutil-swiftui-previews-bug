00902576bcf84e548f403a59079e42f4c4a3a7a6
Andres Riofrio <riofrios@gmail.com>
2020-07-30 18:20:54 -0400

    Initial commit

---
+++ PreviewTest3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PreviewTest3
+//
+//  Created by Andres Riofrio on 7/30/20.
+//  Copyright © 2020 Andres Riofrio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
